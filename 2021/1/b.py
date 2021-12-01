f06f03ad60ab7fa2ce79ed8a746a5b926a19eea0
Keeney <keeney@solve.finance>
2021-12-01 11:00:19 -0500

    Day one!

--- 2021/1/b.py
+++ 2021/1/b.py
@@ -1,12 +1,16 @@
-"""
-Count numbers in an array which are greater than the immediately proceeding
-entry (if any).
-"""
+from collections import deque
+
 FILENAME = "input.txt"
+LOOK_BACK = 3
+
+def filter_empty(item: str) -> bool:
+    return len(item) == 0
 
 if __name__ == "__main__":
+  
     previous = None
-    increase = 0 
+    increase = 0
+    window = deque()
     
     with open(FILENAME, "r") as fid:
         while True:
@@ -16,12 +20,13 @@ if __name__ == "__main__":
                 line = int(text)
             except ValueError:
                 break
+            window.append(line)
+            if len(window) > LOOK_BACK:
+                previous = window.popleft()
             try:
                 if line > previous:
                     increase += 1
             except TypeError:
                 pass
-            previous = line
             
     print("Total count", increase)
-    
