f3d09e28e0aa7048cccbeddc57b22a38335a0adc
Keeney <keeney@solve.finance>
2021-12-03 10:04:34 -0500

    wrong answer

--- 2021/3/b.js
+++ 2021/3/b.js
@@ -5,6 +5,8 @@
 // generate - most common, allow ties, if tie keep 1s
 // scrubber - least common, allow ties, if tie keep 0s
 
+// uh-oh... value of 2002077 is too high
+
 const mostCommon = (arr) => Number(arr.reduce((acc, cur) => acc + 2*cur-1, 0) >= 0);
 const leastCommon = (arr) => Number(arr.reduce((acc, cur) => acc + 2*cur-1, 0) <= 0);
 
@@ -19,11 +21,12 @@ function gen(transpose, mask, modeFcn) {
     return (count === 1 || transpose.length === 0) ? jj : gen(transpose, newMask, modeFcn)
 }
 
-const calc = (modeFcn) => {
-    const data = require("fs").readFileSync("test.txt", 'utf8').split("\n").filter(x=>x).map(row => row.split(""));
-    const transpose = data[0].map((_, colIndex) => data.map(row => parseInt(row[colIndex])));
-    const index = gen(transpose, Array(data.length).fill(true), modeFcn)
-    return parseInt(data[index].join(""), 2)
-}
+["test.txt", "input.txt"].forEach((filename) => {
+    console.log([mostCommon, leastCommon].map(modeFcn => {
+        const data = require("fs").readFileSync(filename, 'utf8').split("\n").filter(x=>x).map(row => row.split(""));
+        const transpose = data[0].map((_, colIndex) => data.map(row => parseInt(row[colIndex])));
+        const index = gen(transpose, Array(data.length).fill(true), modeFcn)
+        return parseInt(data[index].join(""), 2)
+    }).reduce((a,b)=>a*b, 1))
+})
 
-console.log(calc(mostCommon) * calc(leastCommon))
