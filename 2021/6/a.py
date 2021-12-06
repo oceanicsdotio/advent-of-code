4584c4098039b5b18072188976a7af5221c9f2e9
Keeney <keeney@solve.finance>
2021-12-06 13:16:33 -0500

    huzzah!

---
+++ 2021/6/a.py
@@ -0,0 +1,27 @@
+from functools import reduce
+from collections import deque
+from itertools import repeat
+
+CYCLE = 7
+DELAY = 2
+BINS = CYCLE + DELAY
+school = deque(repeat(0, BINS))
+
+def reducer(acc, cur):
+    acc[cur] = acc[cur] + 1
+    return acc
+
+def epoch(school: deque):
+    school.rotate(-1)
+    school[CYCLE-1] += school[BINS-1]
+    
+def evolve(school, epochs):
+    for _ in range(0, epochs):
+        epoch(school)
+    return school
+
+if __name__ == "__main__":
+    with open("input.txt", "r") as fid:
+        fish = map(lambda x: int(x), fid.readline().split(","))
+    print(sum(evolve(reduce(reducer, fish, school), 256)))
+   
